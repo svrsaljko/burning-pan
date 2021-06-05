69a8f64b3917ff33cfe0241eeb8193dd21bd89a0
svrsaljko <vrsaljkostjepan@gmail.com>
2021-06-05 17:27:57 +0200

    burning pan new project initialization

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,4 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <div style={{ textAlign: "center" }}>ŠTO ĆEMO ??</div>
-    {/* <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    /> */}
-    {/* <Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
-  </Layout>
-)
-
-export default IndexPage
+import React from "react"
+export default function Home() {
+  return <h1>Hello Gatsby!</h1>
+}
